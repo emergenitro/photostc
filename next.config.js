f3191515ae38c64e91924515e53256dfe7785293
Karthik Sankar <emergenitro@gmail.com>
2024-07-07 07:26:21 +0800

    Recheck

--- next.config.js
+++ next.config.js
@@ -1,25 +1,10 @@
-// next.config.js
 module.exports = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      child_process: false,
-      net: false,
-      tls: false,
-      crypto: false,
-      dns: false,
-      'timers/promises': false,
-      kerberos: false,
-      zstd: false,
-      '@aws-sdk/credential-providers': false,
-      'gcp-metadata': false,
-      aws4: false,
-      'mongodb-client-encryption': false,
-      snappy: false,
-      socks: false,
-      '@mongodb-js/zstd': false,
-    };
-
-    return config;
-  },
+  async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://api.cloudinary.com/v1_1/:path*',
+        },
+      ]
+    },
 };
